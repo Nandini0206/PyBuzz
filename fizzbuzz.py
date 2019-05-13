526d84656d89c91f11462b865920834e929f9a72
Nandini Patel <nandini_patel96@hotmail.co.uk>
2019-05-13 19:52:14 +0100

    implementing elif statement

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,7 @@
 class FizzBuzz
     if x % 15 == 0:
         print("FizzBuzz")
-    if x % 3 == 0:
+    elif x % 3 == 0:
         print("Fizz")
-    if x % 5 == 0:
+    elif x % 5 == 0:
         print("Buzz")
-            
