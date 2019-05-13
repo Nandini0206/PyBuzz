2bb2c5490b7fba1b00b5a69817a36d22242157fb
Nandini Patel <nandini_patel96@hotmail.co.uk>
2019-05-13 19:55:05 +0100

    added number range of 1 to 100

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,10 @@
 class FizzBuzz
-    if x % 15 == 0:
-        print("FizzBuzz")
-    elif x % 3 == 0:
-        print("Fizz")
-    elif x % 5 == 0:
-        print("Buzz")
-    else:
-        print(x)
+    for x in range(1,101):
+        if x % 15 == 0:
+            print("FizzBuzz")
+        elif x % 3 == 0:
+            print("Fizz")
+        elif x % 5 == 0:
+            print("Buzz")
+        else:
+            print(x)
