84e986fa49e64a0dcd43dd9224b3bb2ee6371206
Nandini Patel <nandini_patel96@hotmail.co.uk>
2019-05-14 12:23:15 +0100

    amended code slightly

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,10 @@
 class FizzBuzz
-    for x in range(1,101):
-        if x % 15 == 0:
-            print("FizzBuzz")
-        elif x % 3 == 0:
+    for num in range(1,101):
+        if num % 3 == 0 and num % 5 == 0:
+            print('FizzBuzz')
+        elif num % 3 == 0:
             print("Fizz")
-        elif x % 5 == 0:
+        elif num % 5 == 0:
             print("Buzz")
         else:
-            print(x)
+            print(num)
